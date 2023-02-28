cecaebadbbe5f53062d4e5b6554ad0dec838034e
kastavich <reachingkhany@gmail.com>
2023-02-28 12:45:52 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -14,7 +14,7 @@ int main(void)
 	int index = 0, sum = 0, diff_half1, diff_half2;
 
 	srand(time(0));
-	while (sum < 277)
+	while (sum < 2772)
 	{
 		password[index] = 33 + rand() % 94;
 		sum += password[index++];
